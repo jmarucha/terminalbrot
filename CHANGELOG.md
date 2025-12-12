d905b2786bf7f22d9c50b4931b58d89571e4b370
Marucha, Jan <jan.marucha@intel.com>
2025-12-12 14:21:31 +0100

    minor fixes and code styling

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# Revision history for etch-a-sketch
+h# Revision history for etch-a-sketch
 
 ## 0.1.0.0 -- YYYY-mm-dd
 
--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,6 @@
 module Main where
 
 import Camera
--- fuck off
-
 import Control.Parallel.Strategies (parMap, rdeepseq, rpar, using)
 import Data.Char (chr, ord)
 import Data.Colour.RGBSpace (uncurryRGB)
@@ -13,7 +11,7 @@ import Numeric (showFFloat)
 import System.Console.ANSI
 import System.IO
 import System.Random (getStdGen, randomR)
-import Prelude hiding (Left, Right)
+import Prelude hiding (Left, Right) -- fuck off
 
 -- stolen from Chris Stryczynski (https://stackoverflow.com/a/38553473)
 getKey :: IO [Char]
@@ -39,13 +37,6 @@ main = do
   mainLoop hue initial_camera
   cleanupScreen
 
-{--
-"\ESC[A" keyUp
-"\ESC[B" keyDown
-"\ESC[C" keyRight
-"\ESC[D" keyLeft
---}
-
 initScreen :: IO ()
 initScreen = do
   hSetBuffering stdin NoBuffering
@@ -61,27 +52,39 @@ cleanupScreen = do
 
 -- definitely not ordinary imperative loop
 mainLoop :: Float -> Camera -> IO ()
-mainLoop hue camera = do
-  let mainLoop' = mainLoop hue
+mainLoop render_hue camera = do
   termSize <- getTerminalSize
   let (h, w) = case termSize of
         Just (h', w') -> (h', w')
-        Nothing -> (25, 80)
+        Nothing -> (24, 80) -- VT-100
   let viewport' = viewport w h 2 camera
       grid = complexGrid w h viewport' `using` rdeepseq
       iter_count = floor (zoom camera * (-50)) :: Integer
-      charDraw = charMap hue iter_count . mandelbrotGraphData iter_count -- at list HLint is happy there is no brackets
+      charDraw = charMap render_hue iter_count . mandelbrotGraphData iter_count
       charsToDraw = concat $ parMap rpar (concatMap charDraw) grid
   setCursorPosition 0 0
   putStr charsToDraw
 
+  let zoomLevelColor = hslColor render_hue 0.8 0.5
+      zoomLevel = exp ((-3) - zoom camera)
+      precision
+        | zoomLevel < 10 = 2
+        | zoomLevel < 1000 = 1
+        | otherwise = 0
   setCursorPosition 0 0
-  setSGR [SetRGBColor Foreground (hslColor hue 0.8 0.5)]
-  putStrLn $ (" x" ++) $ showFFloat (Just 0) (exp ((-3) - zoom camera)) " "
+  setSGR [SetRGBColor Foreground zoomLevelColor]
+  putStrLn $ (" x" ++) $ showFFloat (Just precision) zoomLevel " "
 
   hFlush stdout
 
   key <- getKey
+  let mainLoop' = mainLoop render_hue
+  {--
+  "\ESC[A" keyUp
+  "\ESC[B" keyDown
+  "\ESC[C" keyRight
+  "\ESC[D" keyLeft
+  --}
   case key of
     "w" -> mainLoop' $ move camera Up
     "\ESC[A" -> mainLoop' $ move camera Up
